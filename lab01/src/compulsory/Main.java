831e952caf17d6728e1ab4b720349c26fa15811d
Alexandra719 <alexandracraciun2005@yahoo.com>
2026-02-24 10:02:56 +0200

    Lab 1

---
+++ lab01/src/compulsory/Main.java
@@ -0,0 +1,38 @@
+package compulsory;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        String languages[] = {"C", "C++", "C#", "Phyton", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+
+        int n = (int) (Math.random() * 1_000_000);
+        System.out.println(n);
+
+        int a = n * 3;
+        System.out.println(a);
+
+        a = a + 0b1001;
+        System.out.println(a);
+
+        a = a + 0xFF;
+        System.out.println(a);
+
+        a = a * 6;
+        System.out.println(a);
+
+        int result = a;
+        while(result > 9){
+            int sum = 0;
+            while(result > 0) {
+                sum = sum + result % 10;
+                result = result / 10;
+            }
+            result = sum;
+        }
+        System.out.println(result);
+
+        System.out.println("Willy-nilly, this semester I will learn " + languages[result]);
+    }
+
+}
---
+++ lab01/src/homework/Main.java
@@ -0,0 +1,87 @@
+package homework;
+
+public class Main {
+
+    static void fill(int[][] a, int value) {
+        int n = a.length;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                a[i][j] = value;
+    }
+
+    static void drawRectangle(int[][] a, int height, int width) {
+        int n = a.length;
+        int startRow = (n - height) / 2;
+        int startCol = (n - width) / 2;
+
+        for (int i = startRow; i < startRow + height; i++)
+            for (int j = startCol; j < startCol + width; j++)
+                a[i][j] = 0;
+    }
+
+    static void drawCircle(int[][] a) {
+        int n = a.length;
+        int cx = n / 2;
+        int cy = n / 2;
+        int r = n / 3;
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if ((i - cx) * (i - cx) + (j - cy) * (j - cy) <= r * r)
+                    a[i][j] = 255;
+    }
+
+    public static void main(String[] args) {
+
+        if (args.length < 2) {
+            System.out.println("Usage:");
+            System.out.println("java Main n rectangle");
+            System.out.println("java Main n circle");
+            return;
+        }
+
+        int n = Integer.parseInt(args[0]);
+        String type = args[1];
+
+        long startTime = System.nanoTime();
+
+        int[][] a = new int[n][n];
+
+        if (type.equalsIgnoreCase("rectangle")) {
+
+            fill(a, 255); // fundal alb
+
+            int height = n / 3;
+            int width = n / 2;
+
+            drawRectangle(a, height, width);
+
+        } else if (type.equalsIgnoreCase("circle")) {
+
+            fill(a, 0); // fundal negru
+            drawCircle(a);
+
+        } else {
+            System.out.println("Shape must be rectangle or circle.");
+            return;
+        }
+
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+
+        if (n <= 100) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (a[i][j] == 0)
+                        System.out.print("⬛");
+                    else
+                        System.out.print("⬜");
+                }
+                System.out.println();
+            }
+        }
+
+        System.out.println("Execution time: " + duration + " ns");
+    }
+}
+
